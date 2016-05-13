4629f331f28332628a86d69266c7af4dacac51eb
Richard Slater <github@richard-slater.co.uk>
2016-05-13 21:56:10 +0100

    Added Component Model

---
+++ src/Common/ComponentModel/IChatbotCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentModel
+{
+    public interface IChatbotCommand {
+        bool CanAccept(string receivedText, bool wasMentioned);
+
+        int Priority { get; set; }
+
+        void Process(string command, Action<string> messageHandler);
+    }
+}
