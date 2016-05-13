26bfefa1201d545e651456594d16723a778a5a23
Richard Slater <github@richard-slater.co.uk>
2016-05-13 21:10:14 +0100

    Add Project Structure

---
+++ src/Common/Chatbot/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Chatbot")]
+[assembly: AssemblyDescription("")]
+
+
+[assembly: Guid("f52d1b30-7bc5-4aff-9510-14b50f2e32e8")]
---
+++ src/Slack/CheckyChatbotSlack/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CheckyChatbotSlack")]
+[assembly: AssemblyDescription("")]
+
+
+[assembly: Guid("b26afd64-095b-4fc2-9edb-50c34e6cdd34")]
