bfe34a39948dc5d464286aa83ca2d128a881c37c
Richard Slater <github@richard-slater.co.uk>
2016-05-13 22:27:04 +0100

    Added basic chat bot.

--- src/Slack/CheckyChatbotSlack/Program.cs
+++ src/Slack/CheckyChatbotSlack/Program.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hunabku.Skive;
+using System.Configuration;
 
 namespace CheckyChatbotSlack {
     class Program {
-        static void Main(string[] args) {}
+        static void Main(string[] args) {
+            var configuration = ConfigurationManager.AppSettings;
+            string authToken = configuration["SlackBotToken"];
+
+            var store = new EventHandlersStore()
+                .Register("message", () => new EchoMessageHandler());
+
+            using (var sb = new BotEngine(store))
+            {
+                sb.Connect(authToken).Wait();
+                Console.ReadLine();
+            }
+        }
     }
 }
