3839113e6057cb8cd365a976c8718b93f95b434b
Richard Slater <github@richard-slater.co.uk>
2016-05-13 21:40:10 +0100

    Add high level strructure.

---
+++ src/Common/ComponentModel/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ComponentModel")]
+[assembly: AssemblyDescription("")]
+
+[assembly: Guid("79e8b43c-1f65-4a4d-a640-ab02c438f5a5")]
