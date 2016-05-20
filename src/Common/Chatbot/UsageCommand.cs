f5767db2657f88925891922725d847a46abae067
Richard Slater <github@richard-slater.co.uk>
2016-05-20 21:58:56 +0100

    Added and then disabled Usage command until Ninject.

---
+++ src/Common/Chatbot/UsageCommand.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ComponentModel;
+
+namespace Chatbot {
+    public class UsageCommand : IChatbotCommand {
+        private readonly DateTime _startupTime = DateTime.UtcNow;
+        private int _usageCount;
+
+        public int Priority => 100;
+        public string HelpText => "@checky usage";
+        public string Example => "@checky usage";
+        public string Verb => "usage";
+        public bool CanAccept(string receivedText, bool wasMentioned, bool isDirectMessage) {
+            _usageCount++;
+            var matcher = new Regex("^(?:@?checky|<@[0-9A-Za-z]+>):?\\s([u|U][0-9A-Za-z]*)", RegexOptions.Compiled);
+            return Helpers.CanAcceptWithRegex(receivedText, matcher, "usage");
+        }
+
+        public Task Process(string command, string user, Func<string, Task> responseHandler, IEnumerable<IChatbotCommand> otherCommands) {
+            var delta = DateTime.UtcNow - _startupTime;
+            return
+                responseHandler(
+                    $"I have been operational for {delta.TotalDays:N0} days, {delta.Hours} hours and {delta.Minutes} and in that time I have responded to {_usageCount} requests");
+        }
+    }
+}
