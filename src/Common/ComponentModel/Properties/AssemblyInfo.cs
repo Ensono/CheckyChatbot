ccc3c09bcdb0dc547b03b22737c8d913dcbd5992
Richard Slater <github@richard-slater.co.uk>
2016-05-21 21:16:25 +0100

    Fixing up references and tidying up.

--- src/Common/ComponentModel/Properties/AssemblyInfo.cs
+++ src/Common/ComponentModel/Properties/AssemblyInfo.cs
@@ -1,6 +1,1 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ComponentModel")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("79e8b43c-1f65-4a4d-a640-ab02c438f5a5")]
+﻿
--- src/SmokeBot/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SmokeBot")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("a424692f-25d7-47bd-87c8-5b95bdf314a7")]
