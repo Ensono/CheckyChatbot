71e86d1b4c1028ec67f0eb12a95c68b255af8f69
Richard Slater <github@richard-slater.co.uk>
2016-05-25 18:41:05 +0100

    REFACTORING: Bulk Code Cleanup

--- src/Common/Loader/Validator/DocumentCollectionValidator.cs
+++ src/Common/Loader/Validator/DocumentCollectionValidator.cs
@@ -1,5 +1,4 @@
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using Datastore;
 using Loader.Model;
 
--- src/Common/Loader/Validator/ValidationExtensions.cs
+++ src/Common/Loader/Validator/ValidationExtensions.cs
@@ -8,12 +8,14 @@ namespace Loader.Validator {
             return validator.Validate("Configuration", configuration);
         }
 
-        public static ErrorModel Validate<T>(this CheckyDocument<T> document, string context) where T : PersistentDocument {
+        public static ErrorModel Validate<T>(this CheckyDocument<T> document, string context)
+            where T : PersistentDocument {
             var validator = new DocumentValidator<T>();
             return validator.Validate(context, document);
         }
 
-        public static ErrorModel Validate<T>(this CheckyDocumentCollection<T> documentCollection, string context) where T : PersistentDocument {
+        public static ErrorModel Validate<T>(this CheckyDocumentCollection<T> documentCollection, string context)
+            where T : PersistentDocument {
             var validator = new DocumentCollectionValidator<T>();
             return validator.Validate(context, documentCollection);
         }
