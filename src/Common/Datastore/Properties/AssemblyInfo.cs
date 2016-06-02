1e96eda954fd4fb042600b326d122df87944d498
Richard Slater <github@richard-slater.co.uk>
2016-06-02 23:17:33 +0100

    Added Unit Test Project.

--- src/Common/Datastore/Properties/AssemblyInfo.cs
+++ src/Common/Datastore/Properties/AssemblyInfo.cs
@@ -1,6 +1,1 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Datastore")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("5941d708-1a3e-4f50-9cde-11f81f117bdc")]
+﻿
