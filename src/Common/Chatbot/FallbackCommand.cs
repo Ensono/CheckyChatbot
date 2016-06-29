8cca31cd5ee7cb0f64fabe22a24707d87f550858
Richard Slater <github@richard-slater.co.uk>
2016-06-29 18:29:11 +0100

    Improved support for help in the fallback command.

--- src/Common/Chatbot/FallbackCommand.cs
+++ src/Common/Chatbot/FallbackCommand.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Checky.Common.ComponentModel;
+using Ninject.Infrastructure.Language;
 
 namespace Checky.Common.Chatbot {
     [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
@@ -19,14 +21,50 @@ namespace Checky.Common.Chatbot {
 
         public Task Process(string command, string user, Func<string, Task> responseHandler,
                             IEnumerable<IChatbotCommand> otherCommands) {
-            var possibleVerbs = otherCommands
-                .Select(x => x.Verb)
-                .Where(x => !string.IsNullOrWhiteSpace(x))
-                .Select(x => $"`{x}`")
-                .Aggregate((current, next) => $"{current}, {next}");
-            return
-                responseHandler(
-                    $"Sorry, I didn't understand `{command}`, try: `@checky: <verb> [<arguments>]` where _<verb>_ is one of these: {possibleVerbs}");
+            var verb = command.Split(' ')
+                .First(IsVerb);
+
+            var chatbotCommands = otherCommands
+                .Where(x => !string.IsNullOrWhiteSpace(x.Verb))
+                .ToList();
+
+            var possibleVerbs = chatbotCommands
+                .Where(x => x.Verb.StartsWith(verb))
+                .Select(FormatCommand)
+                .ToList();
+
+            var response = $"Sorry, I didn't understand `{command}`, try: ";
+            if (!possibleVerbs.Any()) {
+                var supportedVerbs = chatbotCommands
+                    .Select(x => x.Verb)
+                    .Where(x => !string.IsNullOrWhiteSpace(x))
+                    .Select(x => $"`{x}`");
+
+                return
+                    responseHandler(
+                        $"{response}`@checky: <verb> [<arguments>]` where _<verb>_ is one of these: {string.Join(", ", supportedVerbs)}");
+            }
+
+            var formattedOutput = string.Join(Environment.NewLine, possibleVerbs);
+                
+            return responseHandler($"{response}{Environment.NewLine}{formattedOutput}");
+        }
+
+        private bool IsVerb(string input) {
+            if (input.StartsWith("@", StringComparison.CurrentCultureIgnoreCase))
+                return false;
+
+            return !input.StartsWith("<@", StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        private static string FormatCommand(IChatbotCommand command) {
+            var builder = new StringBuilder();
+            builder.AppendLine($"*{command.Verb}*");
+            builder.AppendLine("```");
+            builder.AppendLine(command.HelpText);
+            builder.AppendLine($"e.g. {command.Example}");
+            builder.AppendLine("```");
+            return builder.ToString();
         }
     }
 }
