dfcdee3f2f65f4d47e704525b69f8dc01ef36fc1
Richard Slater <github@richard-slater.co.uk>
2016-06-29 21:21:08 +0100

    Fixed up NuGet Packages

---
+++ src/Slack/Checky.Slack.TestEditor/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Checky.Slack.TestEditor.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ src/Slack/Checky.Slack.TestEditor/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Checky.Slack.TestEditor.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
